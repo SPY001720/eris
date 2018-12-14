6fbab0a20b7fac6699e5850cebae1d43c11aeefb
SPY001720 <45824217+SPY001720@users.noreply.github.com>
2018-12-14 15:14:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,39 @@ bot.on('ready', () => {
         if (message.content === "ErisBot"){
             message.reply("Yes ?")
         }
+  
+        if (message.content === prefix+ "Mystik"){
+            var help_embed = new Discord.RichEmbed()
+                .setColor('#B9121B')
+                .addField("Liste des commandes :"," Voici les commandes du Bot !")
+                .addField("!mention", "Cette commande permet de savoir son nom discord en se mentionnant.")
+                .addField("!infos", "Cette commande permet de vous informer sur le serveur.")
+                .addField("!staff", "Cette commande est utile si vous avez besoins de parler a un staff d'un certain grade , elle permet d'afficher la liste des staff de MystikRP ainsi que leur grade.")
+            message.channel.sendEmbed(help_embed);
+        }
     
+        if (message.content === prefix+ "infos"){
+            var help_embed = new Discord.RichEmbed()
+                .setColor('#AEEE00')
+                .addField("Informations :","MystikRP est un serveur Rôleplay qui est composé d'un map personnalisé crée par Pelisma (Mappeur de MystikRP), MystikRP contient parfois ses propres addons !")
+                .addField("Nom du serveur :", "[FR] MYSTIK ROLEPLAY | Map & Addons exclus | LoloxCommunity.com")
+                .addField("IP :", "164.132.116.56:27260")
+                .addField("Gamemode :", "DarkRP")
+                .addField("Site Web :", "https://loloxcommunity.com/")
+            message.channel.sendEmbed(help_embed);
+        }
+    
+        if (message.content === prefix+ "staff"){
+            var help_embed = new Discord.RichEmbed()
+                .setColor('#AEEE00')
+                .addField("Staff du serveur :"," MystikRP")
+                .addField("Fondateur", "Lolox")
+                .addField("Mappeur", "Pelisma")
+                .addField("Superadmins", "Shadow\n Kobralost\n Stylaxx")
+                .addField("Administrateurs", "Skyflix\n Alex Backo\n El Famoso Marvino\n 🔱 𝓢𝓟𝓨 🔱\n ChickenNuggets (Hoie)\n A Fromage de chevre\n Petit enfant arrogant")
+            message.channel.sendEmbed(help_embed);
+        }       
+        
         if (message.content === prefix+ "help"){
             var help_embed = new Discord.RichEmbed()
                 .setColor('#333333')
